61e8cfb1d47a71549e8342b44cc0768da8194f37
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-05-04 23:43:13 +0300

    complete ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,48 +1,58 @@
 #include <iostream>
-#include <string>
+//#include <string>
 #include <fstream>
 
+std::string change_occurrences(std::string base, std::string comp, std::string repl)
+{
+	std::string	str;
+	std::size_t	found = base.find(comp);
+	if (found == std::string::npos)
+		return base;
+	str.append(base, 0, found);
+	str.append(repl);
+	str.append(base.begin() + found + comp.length(), base.end());
+	str = change_occurrences(str, comp, repl);
+	return str;
+}
 
 int main(int argc, char *argv[])
 {
-	std::ifstream	file;
-	std::ifstream	fileReplace;
-	std::string		filename;
-	std::string		str;
-
 	if (argc != 4)
 	{
-		std::cout << "Incorrect number of arguments" << std::endl;
+		std::cerr << "Incorrect number of arguments" << std::endl;
 		return -1;
 	}
+
+	std::string		str;
+	std::string		filename = argv[1];
+	std::string		comp = argv[2];
+	std::string		repl = argv[3];
+	std::ifstream	in;
+	std::ofstream	out;
+
 	//открыть файл
-	filename = argv[1];
-	file.open(filename);
+	in.open(filename);
 	//проверить корректность открытия файла
-	if (!file)
+	if (!in)
 	{
-		std::cout << "Error opening source file" << std::endl;
+		std::cerr << "Error opening source file" << std::endl;
 		return -1;
 	}
 	//создать и открыть новый файл
-	filename.append(".replace");
-	fileReplace.open(filename);
+	out.open(filename.append(".replace"));
 	//проверить, что открыт
-	if (!fileReplace)
+	if (!out)
 	{
-		std::cout << "Error opening target file" << std::endl;
+		std::cerr << "Error opening target file" << std::endl;
 		return -1;
 	}
 	//получаем строку из файла-источника
-	while(!file.eof())
+	while(!in.eof())
 	{
-		getline(file, str);
-		//проверяем на совпадение со строкой S1
-		//если совпадает, то в файл-цель записываем строку S2
-		//иначе запишем полученную строку
-		//когда закончим, сохраним файл и закроем
+		std::getline(in, str);
+		out << change_occurrences(str, comp, repl) << std::endl;
 	}
-	file.close();
-	fileReplace.close();
+	in.close();
+	out.close();
 	return 0;
 }
